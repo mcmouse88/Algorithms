8f61e60c270106e20df9494589c595119c3e8bc6
razdobudko.s <razdobudko.s@wb.ru>
2025-04-27 23:37:42 +0200

    Key-Value to Value-Key

---
+++ hashing/key_value_value_key.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+from typing import List
+
+
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    """
+    ### 347. Top K Frequent Elements
+
+    Given an integer array nums and an integer k, return the k most frequent
+    elements. You may return the answer in any order.
+
+    **Example 1:**
+    > Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+    """
+    pass
