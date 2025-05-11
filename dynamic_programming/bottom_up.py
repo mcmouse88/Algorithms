a9361188ec53bcb00eb4ae1ed595ea48e10dd58e
razdobudko.s <razdobudko.s@wb.ru>
2025-05-11 23:19:39 +0200

    Dynamic Programming

---
+++ dynamic_programming/bottom_up.py
@@ -0,0 +1,43 @@
+"""
+
+"""
+from typing import List
+
+
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    """
+    ### 1143. Longest Common Subsequence
+
+    Given two strings text1 and text2, return the length of their longest common
+    subsequence. If there is no common subsequence, return 0.
+
+    A subsequence of a string is a new string generated from the original string
+    with some characters (can be none) deleted without changing the relative order
+    of the remaining characters.
+
+    For example, "ace" is a subsequence of "abcde".
+    A common subsequence of two strings is a subsequence that is common to both
+    strings.
+
+    **Example 1:**
+    > Input: text1 = "abcde", text2 = "ace"
+    Output: 3
+    Explanation: The longest common subsequence is "ace" and its length is 3.
+    """
+    pass
+
+
+def lengthOfLIS(nums: List[int]) -> int:
+    """
+    ### 300. Longest Increasing Subsequence
+
+    Given an integer array nums, return the length of the longest strictly
+    increasing subsequence.
+
+    **Example 1:**
+    > Input: nums = [10,9,2,5,3,7,101,18]
+    Output: 4
+    Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+    the length is 4.
+    """
+    pass
